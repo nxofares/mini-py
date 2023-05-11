9f3d2a94ebc627571c9ac974af7ad7cec1d94ac3
N. FARES <nxofares@gmail.com>
2023-05-11 14:38:41 -0700

    Add files via upload

---
+++ 12 python projects/guess_number/main.py
@@ -0,0 +1,13 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input("guess a number between 1 and {x}"))
+        if guess < random_number:
+            print("sorry, guess again too low")
+        elif guess > random_number:
+            print("sorry, guess again too high")
+    print("yay congrats you guessed")
